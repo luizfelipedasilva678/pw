f52d23ab3aa0c2a613d3296a092a350771826419
Luiz Felipe <luizfelipedasilva678@gmail.com>
2023-03-18 15:08:56 -0300

    Exercicio 6

---
+++ aula3/exe6.php
@@ -0,0 +1,5 @@
+<?php 
+
+    $dados = [ 'carro', 'carro', 'caminhão', 'caminhão','bicicleta', 'caminhada',  'carro',  'van',  'bicicleta',  'caminhada',  'carro', 'van', 'carro', 'caminhão' ];
+
+    print_r(array_count_values($dados));
